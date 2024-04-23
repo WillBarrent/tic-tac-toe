27030c8bffc34e3b33d50430c2fae5ba29267a7c
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-04-23 20:14:52 +0500

    Finish

--- index.js
+++ index.js
@@ -1,86 +1,217 @@
-let createGameBoard = (function() {
-    let gameBoard = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
+// Create Game Board
+let createGameBoard = (function () {
+  const gameBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  const generateGameBoard = () => {
+    const X = "x";
+    const O = "o";
+
+    gameBoard = [
+      ["", X, O],
+      ["", X, O],
+      ["", X, ""],
     ];
 
-    let generateGameBoard = () => {
-        const X = 'x';
-        const O = 'o'; 
+    return gameBoard;
+  };
 
-        gameBoard = [
-            ['', X, O],
-            ['', X, O],
-            ['', X, '']
-        ];
+  const interface = (() => gameBoard)();
 
-        return gameBoard;
-    };
-
-    let interface = () => gameBoard;
-
-    return { interface, generateGameBoard };
+  return { interface, generateGameBoard };
 })();
 
-let checkWinner = (function(gameBoard) {
-    // ----------------------------------
-    let checkFor = function(conditions, arr, direction) {
-        let letter = '';
-        let counter = 0;
-        for (let i = 0; i < 3; i++) {
-            counter = 0; 
-            for (let j = 0; j < 2; j++) {
-                if (((direction == 'row') ? arr[i][j] != '' : arr[j][i] != '') && ((direction === 'row' && arr[i][j] == arr[i][j+1]) || (direction === 'column') && arr[j][i] == arr[j+1][i])) {
-                    conditions = true;
-                    counter += 1;
-                    letter = (direction == 'row') ? arr[i][j] : arr[j][i];
-                    if (conditions === true && counter === 2) break;
-                }
-                else {
-                    conditions = false;
-                }
-            }
-            if (conditions === true && counter === 2) break;
+// Check for winner (logic)
+const checkWinner = function (gameBoard) {
+  // ----------------------------------
+  let checkFor = function (conditions, arr, direction) {
+    let letter = "";
+    let counter = 0;
+    for (let i = 0; i < 3; i++) {
+      counter = 0;
+      for (let j = 0; j < 2; j++) {
+        if (
+          (direction == "row" ? arr[i][j] != "" : arr[j][i] != "") &&
+          ((direction === "row" && arr[i][j] == arr[i][j + 1]) ||
+            (direction === "column" && arr[j][i] == arr[j + 1][i]))
+        ) {
+          conditions = true;
+          counter += 1;
+          letter = direction == "row" ? arr[i][j] : arr[j][i];
+          if (conditions === true && counter === 2) break;
+        } else {
+          conditions = false;
         }
-        return [conditions === true && counter === 2, letter];
+      }
+      if (conditions === true && counter === 2) break;
     }
+    return [conditions === true && counter === 2, letter];
+  };
 
-    let checkForRows = function() {
-        let [rowConditions, letter] = checkFor(false, gameBoard, 'row');
-        
-        return rowConditions && `${letter} is winner!`;
-    }
+  let checkForRows = function () {
+    let [rowConditions, letter] = checkFor(false, gameBoard, "row");
 
-    let checkForColumns = function() {
-        let [columnConditions, letter] = checkFor(false, gameBoard, 'column');
+    return rowConditions && `${letter}`;
+  };
 
-        return columnConditions && `${letter} is winner!`;
-    }
+  let checkForColumns = function () {
+    let [columnConditions, letter] = checkFor(false, gameBoard, "column");
 
-    let checkForDiagonals = function() {
-        let diagonalConditions = (((gameBoard[0][0] === gameBoard[1][1]) && (gameBoard[1][1] === gameBoard[2][2])) && gameBoard[0][0]) || ((gameBoard[0][2] === gameBoard[1][1]) && (gameBoard[1][1] === gameBoard[2][0]) && gameBoard[0][2]);
+    return columnConditions && `${letter}`;
+  };
 
-        return diagonalConditions && `${diagonalConditions} is winner!`; 
-    }
+  let checkForDiagonals = function () {
+    let diagonalConditions =
+      (gameBoard[0][0] === gameBoard[1][1] &&
+        gameBoard[1][1] === gameBoard[2][2] &&
+        gameBoard[0][0]) ||
+      (gameBoard[0][2] === gameBoard[1][1] &&
+        gameBoard[1][1] === gameBoard[2][0] &&
+        gameBoard[0][2]);
 
-    if (gameBoard instanceof Array) {
-        let conditions = [checkForRows(), checkForColumns(), checkForDiagonals()];;
+    return diagonalConditions && `${diagonalConditions}`;
+  };
 
-        return conditions;
-    }
+  if (gameBoard instanceof Array) {
+    let conditions = [checkForRows(), checkForColumns(), checkForDiagonals()];
 
-    throw new Error("Gameboard is not an array!");
-});
+    return conditions;
+  }
+
+  throw new Error("Gameboard is not an array!");
+};
+
+// Factory function for DOM manipulation of the game
+// I create it because i want to isolate a logic from the global scope
+
+function gameInteractive(gameBoardArray) {
+  // Initializtion
+  const X = "x";
+  const O = "o";
+
+  const game = document.querySelector(".game");
+  const gameStart = document.querySelector(".game-start");
+  const gameBoard = document.querySelector(".gameboard");
+  const playerOneName = document.querySelector(".player-one-name");
+  const playerTwoName = document.querySelector(".player-two-name");
+  const initializtion = document.querySelector(".initialization");
+
+  // Helper functions
+  const classAdd = function (element, addClass) {
+    element.classList.add(addClass);
+  };
+  const classRemove = function (element, removeClass) {
+    element.classList.remove(removeClass);
+  };
+  const classContain = function (element, containClass) {
+    return element.classList.contains(containClass);
+  };
+
+  //   Functions
+  function createGameBoard() {
+    classRemove(gameBoard, "hidden");
+
+    gameBoardArray.forEach((array) => {
+      array.forEach((cell) => {
+        let letter = cell;
+
+        let div = document.createElement("div");
+        div.className = "cell";
+        div.textContent = letter == "x" || letter == "y" ? letter : " ";
+
+        gameBoard.appendChild(div);
+      });
+    });
+  }
+
+  function createArrayFromGameBoard(DOMGameBoard) {
+    const oneDimensionGameBoard = Array.from(DOMGameBoard).map(cell => cell.textContent);
+    const twoDimensionGameBoard = [[...oneDimensionGameBoard.slice(0, 3)], [...oneDimensionGameBoard.slice(3, 6)], [...oneDimensionGameBoard.slice(6)]];
+    
+    return twoDimensionGameBoard;
+  }
+
+  function playerTitle(firstPlayer, secondPlayer) {
+    let div = document.createElement("div");
+    div.className = "player";
+    div.innerHTML = `
+        <div class="player-one">Player(X): ${firstPlayer}</div>
+        <span class="dash">-</span>
+        <div class="player-two">Player(O): ${secondPlayer}</div>
+    `;
+    game.appendChild(div);
+  }
+
+  function displayWinner(winner) {
+    let div = document.createElement('div');
+    div.className = 'game-end';
+    div.innerHTML = `
+      <div class="win-or-lose">Winner is: ${winner}</div>
+      <div>Refresh the page for restart the game!</div>
+    `;
+    game.appendChild(div);
+  }
+
+  function gameInitialization() {
+    let firstPlayer = "None",
+      secondPlayer = "none";
+
+    initializtion.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      firstPlayer = playerOneName.value;
+      secondPlayer = playerTwoName.value;
+
+      if (firstPlayer && secondPlayer) {
+        classAdd(gameStart, "hidden");
+        createGameBoard();
+        playerTitle(firstPlayer, secondPlayer);
+      }
+    });
+
+    return [firstPlayer, secondPlayer];
+  }
+
+  function gameInteraction() {
+    const X = "x";
+    const O = "o";
+    let order = true;
+    let theEnd = false;
+
+    gameBoard.addEventListener("click", function (e) {
+      let cell = e.target;
+
+      if (!classContain(cell, "cellIsCount") && !theEnd) {
+        cell.textContent = order ? X : O;
+        classAdd(cell, order ? "cell-x" : "cell-o");
+        classAdd(cell, "cellIsCount");
+        order = !order;
+        const DOMGameBoard = document.querySelectorAll('.gameboard > .cell');
+        let checkForWinner = checkWinner(createArrayFromGameBoard(DOMGameBoard));
+        if (checkForWinner.find(cell => cell === X || cell === O)) {
+          let XWin = checkForWinner.find(cell => cell === X);
+          displayWinner(XWin ? document.querySelector('.player-one').textContent : document.querySelector('.player-two').textContent);
+
+          theEnd = true;
+        }
+      }
+    });
+  }
+
+  return { gameInitialization, gameInteraction };
+}
 
 // Factory function for control flow of the game
 // I create it because i want to isolate a logic from the global scope
 function game() {
-    let gameBoard = createGameBoard;
+  const gameBoard = createGameBoard;
+  const DOMInteraction = gameInteractive(gameBoard.interface);
 
-    createGameBoard.generateGameBoard();
-
-    console.log(checkWinner(createGameBoard.interface()));
+  DOMInteraction.gameInitialization();
+  DOMInteraction.gameInteraction();
 }
 
 game();
